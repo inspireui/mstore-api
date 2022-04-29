d96500737a81f60d1ba64446aeda2d97a847d30c
Truong Nguyen <minhtruong315@gmail.com>
2022-04-29 16:32:38 +0700

    add membershio

--- mstore-api/mstore-api.php
+++ mstore-api/mstore-api.php
@@ -26,6 +26,7 @@ include_once plugin_dir_path(__FILE__) . "controllers/flutter-delivery.php";
 include_once plugin_dir_path(__FILE__) . "functions/index.php";
 include_once plugin_dir_path(__FILE__) . "functions/utils.php";
 include_once plugin_dir_path(__FILE__) . "controllers/flutter-tera-wallet.php";
+include_once plugin_dir_path(__FILE__) . "controllers/flutter-membership/index.php";
 include_once plugin_dir_path(__FILE__) . "controllers/flutter-paytm.php";
 include_once plugin_dir_path(__FILE__) . "controllers/flutter-paystack.php";
 include_once plugin_dir_path(__FILE__) . "controllers/flutter-flutterwave.php";
