bffa04f1feed4913a11f221a0eb9a048da11b459
Truong <minhtruong315@gmail.com>
2024-03-27 16:53:05 +0700

    update listing api to support query by featured

--- mstore-api/mstore-api.php
+++ mstore-api/mstore-api.php
@@ -3,7 +3,7 @@
  * Plugin Name: MStore API
  * Plugin URI: https://github.com/inspireui/mstore-api
  * Description: The MStore API Plugin which is used for the MStore and FluxStore Mobile App
- * Version: 4.12.7
+ * Version: 4.12.8
  * Author: InspireUI
  * Author URI: https://inspireui.com
  *
@@ -53,7 +53,7 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 
 class MstoreCheckOut
 {
-    public $version = '4.12.7';
+    public $version = '4.12.8';
 
     public function __construct()
     {
