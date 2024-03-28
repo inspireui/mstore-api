39735dc1e7cbc1862717b46681fe41b39f63f931
Truong <minhtruong315@gmail.com>
2024-03-28 15:52:56 +0700

    upgrade v12

--- mstore-api/controllers/flutter-membership/index.php
+++ mstore-api/controllers/flutter-membership/index.php
@@ -67,32 +67,41 @@ class FlutterMembership extends FlutterBaseController
     public function membership_register()
     {
         if (is_plugin_active('indeed-membership-pro/indeed-membership-pro.php')) {
-            if (!class_exists('FlutterUserAddEdit')) {
-                require_once(__DIR__ . '/flutter-user-add-edit.class.php');
+            if (!class_exists('FlutterRegisterForm')) {
+                require_once(__DIR__ . '/flutter-register-form.php');
             }
             $nonce = wp_create_nonce('ihc_user_add_edit_nonce');
             $json = file_get_contents('php://input');
             $_POST = json_decode($json, TRUE);
-            $_POST['ihc_user_add_edit_nonce'] = $nonce;
-            $_REQUEST['ihc_payment_gateway_radio'] = sanitize_text_field($_POST['ihc_payment_gateway_radio']);
-            $_REQUEST['ihc_payment_gateway'] = sanitize_text_field($_POST['ihc_payment_gateway']);
-            $args = array(
-                'user_id' => false,
-                'type' => 'create',
-                'tos' => true,
-                'captcha' => true,
-                'action' => '',
-                'is_public' => true,
-                'url' => get_home_url(),
-            );
-            $obj = new FlutterUserAddEdit();
-            $obj->setVariable($args);//setting the object variables
-            $res = $obj->save_update_user();
-            if (is_wp_error($res)) {
-                return $res;
-            } else {
-                return $res;
+            $actionValue = 'register';
+            $postData = [
+                'user_login' => $_POST['user_login'],
+                'user_email' => $_POST['user_email'],
+                'first_name' => $_POST['first_name'],
+                'last_name' => $_POST['last_name'],
+                'pass1' => $_POST['pass1'],
+                'pass2' => $_POST['pass2'],
+                'ihc_country' => $_POST['ihc_country'] ?? 'US',
+                'ihc_avatar' => $_POST['ihc_avatar'] ?? '',
+                'tos' => $_POST['tos'],
+                // 'code' => $_POST['code'],
+                // 'csrf' => $_POST['csrf'],
+                // 'digit_otp' => $_POST['digit_otp'],
+                'ihcFormType' => $_POST['ihcFormType'],
+                'ihcaction' => $_POST['ihcaction'],
+                'ihc_user_add_edit_nonce' => $nonce,
+                'lid' => $_POST['lid'],
+                'payment_selected' => $_POST['ihc_payment_gateway'],
+                'checkout-form' => 1
+            ];
+            $obj = new FlutterRegisterForm();
+            $res = $obj->save($actionValue, $postData);
+            $errors = $obj->getErrors();
+            if(count($errors)){
+                $keys = array_keys($errors);
+                return parent::sendError($keys[0], $errors[$keys[0]], 400);
             }
+            return $res;
         } else {
             return parent::sendError("plugin_not_found", "Please install Membership Pro Ultimate WP", 404);
         }
