145f9c28e8da4d38db0d95ec6ab9af1a6fb3c260
Truong <minhtruong315@gmail.com>
2024-10-11 09:58:45 +0700

    up version

--- mstore-api/mstore-api.php
+++ mstore-api/mstore-api.php
@@ -3,7 +3,7 @@
  * Plugin Name: MStore API
  * Plugin URI: https://github.com/inspireui/mstore-api
  * Description: The MStore API Plugin which is used for the FluxBuilder and FluxStore Mobile App
- * Version: 4.15.4
+ * Version: 4.15.5
  * Author: FluxBuilder
  * Author URI: https://fluxbuilder.com
  *
@@ -56,7 +56,7 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 
 class MstoreCheckOut
 {
-    public $version = '4.15.4';
+    public $version = '4.15.5';
 
     public function __construct()
     {
